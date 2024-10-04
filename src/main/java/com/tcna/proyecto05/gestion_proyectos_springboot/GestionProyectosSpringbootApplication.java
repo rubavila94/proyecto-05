a1ff3eabb87da73c2bdcbd50071ce8a6a10e0f1a
rubavila94 <rubavila94@gmail.com>
2024-10-04 12:26:49 +0200

    first commit proyecto05

---
+++ src/main/java/com/tcna/proyecto05/gestion_proyectos_springboot/GestionProyectosSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.tcna.proyecto05.gestion_proyectos_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProyectosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProyectosSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcna/proyecto05/gestion_proyectos_springboot/GestionProyectosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcna.proyecto05.gestion_proyectos_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProyectosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
